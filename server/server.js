239df63ae2540653017c2f53e0804f2b7d5bb660
khush239 <khushshah9990@gmail.com>
2026-03-11 18:39:13 +0530

    fix: Vercel CORS config and client routing rewrite

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const app = express()
 await connectDB()
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.get('/', (req, res)=> res.send("Server is running"))
